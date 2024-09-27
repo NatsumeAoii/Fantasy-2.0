3b7b9dc01e5c61c4a6777a5d570b575bb7faf231
Natsume <wardanadm2@gmail.com>
2024-09-28 00:54:04 +0700

    fix bug

--- js/data/roleTitles.js
+++ js/data/roleTitles.js
@@ -1,4 +1,4 @@
-export const roleTitles = {
+export const titles = {
     //Higest Tier Roles
     "Archmage": [
         "Master of Arcane Mysteries", "Keeper of the Eternal Flame", "Grand Weaver of Spells", 
